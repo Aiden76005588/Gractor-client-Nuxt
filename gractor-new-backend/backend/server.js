a53684971b756d778b425755430a45bb4c753207
Aiden76005588 <fsd76005588@gmail.com>
2021-03-29 22:53:15 +0900

    [server] express+socket+mongodb

--- gractor-new-backend/backend/server.js
+++ gractor-new-backend/backend/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
+const http = require("http");
+const socketIo = require("socket.io");
+const request = require("request");
 
 const usersRouter = require("./routes/users");
 const path = require("path");
@@ -22,14 +25,36 @@ db.once("open", () => console.log("Connected to DB!"));
 
 app.use(express.json());
 app.use(cors());
-
+app.get("/", (req, res) => res.send("Hello world"));
 app.use("/api/users", usersRouter);
 
 // app.use(express.static(path.join(__dirname, '../frontend/build')));
 // app.get('*', (req, res) =>
 //   res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 // );
+const server = http.Server(app);
+server.listen(3000, () => console.log("hello"));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  setInterval(() => {
+    request(
+      {
+        method: "GET",
+        uri: "https://api.upbit.com/v1/ticker?markets=KRW-BTC",
+      },
+      (err, result) => {
+        console.log(err);
+        console.log(JSON.parse(result.body));
+        console.log("hello============");
+        const parsedData = JSON.parse(result.body);
+        socket.emit("data", parsedData);
+      }
+    );
+  }, 4000);
 });
